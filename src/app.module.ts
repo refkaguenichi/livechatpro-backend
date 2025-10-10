82d4736520313f067f56ee16557beeec670939fb
Refka <g.refka@cynoia.com>
2025-10-10 10:06:00 +0100

    authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ChatModule } from './chat/chat.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot(),
+      TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',         
+      port: 3306,                
+      username: 'root',          
+      password: '',      
+      database: 'livechatpro',   
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,        
+    }),
+      UserModule,
+      AuthModule,
+      ChatModule,
+      DashboardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
